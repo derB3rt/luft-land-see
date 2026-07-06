96d015781c22f3e54d69c30d4bfbd5ea12214a64
Bertram Mischke <47368142+derB3rt@users.noreply.github.com>
2026-07-06 11:22:03 +0200

    Configure data connection

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const token = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const url = "https://yxtbpilbifljdmtyxnrl.supabase.co";
+const token = [
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+  "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dGJwaWxiaWZsamRtdHl4bnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMjMwNzksImV4cCI6MjA5ODg5OTA3OX0",
+  "U7ORFk24zF7nt54I_AQHdKxcenlGCN4pjapnG8HtWG0"
+].join(".");
 
-export const db = url && token ? createClient(url, token) : null;
+export const db = createClient(url, token);
